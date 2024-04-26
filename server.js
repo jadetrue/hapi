ea4b66bb825fe79232e634c77ceb32fd8948a414
Jade True <jadetruexo@gmail.com>
2024-04-26 09:41:13 +0100

    init hapi server

---
+++ server.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+
+  // adding routes
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (req, h) => {
+        return 'Hello, Jade'
+    }
+  })
+
+  await server.start();
+  console.log(`Server is running on ${server.info.uri}`);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
